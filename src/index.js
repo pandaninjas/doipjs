b78d7a7ce9189d53adecbd43f67d4238084aab26
Yarmo Mackenbach <yarmo@yarmo.eu>
2020-10-24 11:19:35 +0200

    Add URI validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-const verify = (url, fingerprint, opts) => {
+const validUrl = require('valid-url')
+const serviceprovidersList = require('./serviceproviders').serviceprovidersList
 
+const verify = (uri, fingerprint, opts) => {
+  if !(validUrl.isUri(uri)) {
+    throw new Error('The provided URI was not valid')
+  }
 }
 
 exports.verify = verify
 exports.serviceproviders = require('./serviceproviders').serviceproviders
-exports.serviceprovidersList = require('./serviceproviders').serviceprovidersList
+exports.serviceprovidersList = serviceprovidersList
