82852217fbf58aeaec35cf99b062d5c732b1c921
Yarmo Mackenbach <yarmo@yarmo.eu>
2020-11-04 22:38:03 +0100

    Add liberapay service provider

---
+++ src/serviceproviders/liberapay.js
@@ -0,0 +1,64 @@
+/*
+Copyright 2020 Yarmo Mackenbach
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+const reURI = /^https:\/\/liberapay\.com\/(.*)\/?/
+
+const processURI = (uri, opts) => {
+  if (!opts) { opts = {} }
+  const match = uri.match(reURI)
+
+  return {
+    serviceprovider: {
+      type: 'web',
+      name: 'liberapay'
+    },
+    profile: {
+      display: match[1],
+      uri: uri
+    },
+    proof: {
+      uri: uri,
+      fetch: `https://liberapay.com/${match[1]}/public.json`,
+      useProxy: false,
+      format: 'json'
+    },
+    claim: {
+      fingerprint: null,
+      format: 'message',
+      path: ['statements', 'content'],
+      relation: 'contains'
+    },
+    qr: null
+  }
+}
+
+const tests = [
+  {
+    uri: 'https://liberapay.com/alice',
+    shouldMatch: true
+  },
+  {
+    uri: 'https://liberapay.com/alice/',
+    shouldMatch: true
+  },
+  {
+    uri: 'https://domain.org/alice',
+    shouldMatch: false
+  }
+]
+
+exports.reURI = reURI
+exports.processURI = processURI
+exports.tests = tests
