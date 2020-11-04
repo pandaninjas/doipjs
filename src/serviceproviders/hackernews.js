a9fc0e4b445af06b287cd154bf1b49f19be50b3a
Yarmo Mackenbach <yarmo@yarmo.eu>
2020-11-04 22:25:45 +0100

    Fix dots in regex

--- src/serviceproviders/hackernews.js
+++ src/serviceproviders/hackernews.js
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-const reURI = /^https:\/\/news.ycombinator.com\/user\?id=(.*)\/?/
+const reURI = /^https:\/\/news\.ycombinator\.com\/user\?id=(.*)\/?/
 
 const processURI = (uri, opts) => {
   if (!opts) { opts = {} }
--- src/serviceproviders/lobsters.js
+++ src/serviceproviders/lobsters.js
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-const reURI = /^https:\/\/lobste.rs\/u\/(.*)\/?/
+const reURI = /^https:\/\/lobste\.rs\/u\/(.*)\/?/
 
 const processURI = (uri, opts) => {
   if (!opts) { opts = {} }
