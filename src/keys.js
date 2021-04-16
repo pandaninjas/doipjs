b80e4c50bb5c89565a9e7e268ba6fb1e3ab9f893
Yarmo Mackenbach <yarmo@yarmo.eu>
2021-04-16 13:10:25 +0200

    Remove obsolete imports

--- src/keys.js
+++ src/keys.js
@@ -13,12 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-const path = require('path')
 const bent = require('bent')
 const req = bent('GET')
 const validUrl = require('valid-url')
 const openpgp = require('openpgp')
-const mergeOptions = require('merge-options')
 
 const fetchHKP = (identifier, keyserverBaseUrl) => {
   return new Promise(async (resolve, reject) => {
