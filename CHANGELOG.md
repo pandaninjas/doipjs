dfcb29b0d1a953fdfa59ede5fcea778fdb6aeea1
Yarmo Mackenbach <yarmo@yarmo.eu>
2021-11-15 16:16:50 +0100

    Add examples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Examples
+
 ## [0.14.0] - 2021-11-06
 ### Added
 - Support for ariadne.id notation proofs
---
+++ examples/fetch-key-wkd.js
@@ -0,0 +1,14 @@
+const doip = require('../src')
+
+const main = async () => {
+    // Fetch the key using WKD
+    const key = await doip.keys.fetchWKD("test@doip.rocks")
+
+    // Process it to extract the UIDs and their claims
+    const obj = await doip.keys.process(key)
+
+    // Log the claims of the first UID
+    console.log(obj.users[0].claims)
+}
+
+main()
