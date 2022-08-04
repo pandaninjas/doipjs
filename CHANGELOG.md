d61d0873bf458fa8ad1c06312f63e632a45fa295
Yarmo Mackenbach <yarmo@yarmo.eu>
2022-08-04 10:32:15 +0200

    Support another vCard syntax

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [0.15.6] - 2022-03-27
 ### Changed
 - Gitlab claims now use HTTP fetcher (thank [hosaka](https://codeberg.org/hosaka)!)
 ### Removed
 - Obsolete Gitlab proof fetcher
+### Fixed
+- Support another XMPP vCard syntax
 
 ## [0.15.6] - 2022-03-27
 ### Added
--- src/fetcher/xmpp.js
+++ src/fetcher/xmpp.js
@@ -110,7 +110,10 @@ if (jsEnv.isNode) {
         switch (data.field.toLowerCase()) {
           case 'desc':
           case 'note':
-            vcard = dom.window.document.querySelector('note text')
+            vcard = dom.window.document.querySelector('note')
+            if (!vcard) {
+              vcard = dom.window.document.querySelector('note text')
+            }
             if (!vcard) {
               vcard = dom.window.document.querySelector('DESC')
             }
