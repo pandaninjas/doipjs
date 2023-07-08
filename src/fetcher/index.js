92d150efead740557748dd7025559b22080ee80c
Yarmo Mackenbach <yarmo@yarmo.eu>
2023-07-08 08:46:02 +0200

    fix: remove obsolete dep

--- src/fetcher/index.js
+++ src/fetcher/index.js
@@ -13,9 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import dotenv from 'dotenv'
-dotenv.config()
-
 export * as activitypub from './activitypub.js'
 export * as dns from './dns.js'
 export * as graphql from './graphql.js'
